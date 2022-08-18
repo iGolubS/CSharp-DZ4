edb6fffe5d390b679c3d801c9ab70c8b63f82642
iGolubS <109303592+iGolubS@users.noreply.github.com>
2022-08-18 10:08:02 +0300

    Update dz4.cs

--- dz4.cs
+++ dz4.cs
@@ -1,14 +1,15 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)    2, 4 -> 16
 
-//  Console.Write("input number A ");
-//  int numA = Convert.ToInt32(Console.ReadLine());
-//  Console.Write("input number B ");
-//  int numB = Convert.ToInt32(Console.ReadLine());
+//  Console.Clear();
+//  Console.Write("Введите число A: ");
+//  int A = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Введите число B: ");
+//  int B = Convert.ToInt32(Console.ReadLine());
 //  int num = 1;
-//  for (int i = 0; i < numB; i++)
+//  for (int i = 0; i < B; i++)
 //  {
-//     num*= numA;
+//     num*= A;
 //  }
 //  Console.Write(num);
 
